a28b8041086e05c6c1e5188546b7ad1f78b4c3bb
Anna Kwadrans <annakwadrans@MacBook-Air-de-Anna.local>
2026-05-24 17:17:47 +0200

    notes

---
+++ nestedClasses.cpp
@@ -0,0 +1,36 @@
+class Cat
+{
+private:
+        class Leg
+        {
+        private:
+                /* data */
+        public:
+                Leg(/* args */);
+                ~Leg();
+        };
+
+};
+
+class Dog
+{
+private:
+        class Leg
+        {
+                private:
+
+                public:
+
+        };
+};
+
+
+
+
+
+int main(void)
+{
+        Cat             aCat;
+        Cat::Leg        aCatsLeg;
+
+}
